20d52a24dad5bb347072237b2ab156bd5c1970e4
SQY USER <jwala.maurya@squareyards.co.in>
2023-07-19 18:34:09 +0530

    table

---
+++ bin/while_loop.dart/while_table.dart
@@ -0,0 +1,11 @@
+//Generate a multiplication table for a given number using while loop
+void main() {
+  int num = 5;
+  int i = 1;
+
+  while (i <= 10) {
+    int table = i * num;
+      print(table);
+    i++;
+  }
+}
